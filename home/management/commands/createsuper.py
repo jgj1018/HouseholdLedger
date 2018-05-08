6b287bb67fa2c0951a326f03fa99dd018cf4782a
gring2 <pjw6454@hotmail.com>
2018-05-08 16:54:48 +0900

    check whether db is connectable

--- home/management/commands/createsuper.py
+++ home/management/commands/createsuper.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        print('Create Super-User')
         User.objects.create_superuser('admin','admin@admin.com','gring21!')
