8613e9a1364675af3d444492d2b442f29da6b5bb
gring2 <pjw6454@hotmail.com>
2018-05-12 20:59:35 +0900

    Make model of transaction

---
+++ src/components/AccountingInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="input">
+    <h3>INPUT SECTION</h3>
+    <input type="text">
+    <input type="text">
+
+    <button>SUBMIT</button>
+    <button id="test" @click="show">Test</button>
+    <div id="result">{{test}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      test: ''
+    }
+  },
+  methods: {
+    show: function () {
+      this.test = 'GETIT'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
