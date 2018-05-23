1bf384279c55f90e55627add239b13fda7c657fe
jgj1018 <jgj1018@gmail.com>
2018-05-23 23:48:28 +0900

    (1) delete unused requirement (2) add default model schema

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+  user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  transaction_id = models.BigAutoField()
+  transaction_name = models.CharField(max_length=20)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now_add=True)
+
+class DebitSide(models.Model):
+  transaction_id = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+  cost_amount = models.IntegerField()
+  transaction_type = models.SmallIntegerField()
+
+class CreditSide(models.Model):
+  transaction_id = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+  cost_amount = models.IntegerField()
+  transaction_type = models.SmallIntegerField()
