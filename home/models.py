eacade908068b99d5d04a79ab3cbdbae7f66bb79
jgj1018 <jgj1018@gmail.com>
2018-05-26 00:04:17 +0900

    (1) add transaction model serializer

--- home/models.py
+++ home/models.py
@@ -6,10 +6,10 @@ class Transaction(models.Model):
     user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     transaction_id = models.BigAutoField()
     transaction_name = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True)
     cost_amount = models.IntegerField()
     transaction_type = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True)
 
     class Meta:
         db_table = 'transaction'
--- home/serializer.py
+++ home/serializer.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from home.models import Transaction
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url',
+                  'username',
+                  'email',
+                  'groups'
+                  )
 
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('user_id',
+                  'transaction_id',
+                  'transaction_name',
+                  'cost_amount',
+                  'transaction_type',
+                  'created_at',
+                  'updated_at',
+                  )
