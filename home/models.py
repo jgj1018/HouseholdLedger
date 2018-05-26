1fd5e7585387236c24ab0f63af35db00ca151a11
gring2 <pjw6454@hotmail.com>
2018-05-26 10:17:08 +0900

    change model field (Build check done)

--- home/models.py
+++ home/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.urls import reverse
 
+
 # Whenever any transaction happens, The row is added to this table.
+
+
 class Transaction(models.Model):
     user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    transaction_id = models.BigAutoField()
+    # Primary Key necessary, Or auto generate Id column
+    # Only Primary Key can be autoField
+    transaction_id = models.BigAutoField(primary_key=True)
     transaction_name = models.CharField(max_length=20)
     cost_amount = models.IntegerField()
     transaction_type = models.SmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True)
+    # only one auto option can be set
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'transaction'
@@ -20,6 +26,5 @@ class Transaction(models.Model):
         return self.transaction_name
 
     def get_absolute_url(self):
-        #TODO have to set what page should be next.
+        # TODO have to set what page should be next.
         return reverse('', args=[str(self.user_id)])
-
