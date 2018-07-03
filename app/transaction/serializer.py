5741aa43a6cefe0c045625eae3cd9a99856e7a42
jgj1018 <jgj1018@gmail.com>
2018-07-03 22:41:55 +0900

    (1) separate transaction instance from home test complete
    * be care that the log path is changed.

---
+++ app/transaction/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from transaction.models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('user_id',
+                  'transaction_id',
+                  'transaction_name',
+                  'cost_amount',
+                  'transaction_type',
+                  'created_at',
+                  'updated_at',
+                  )
