d39c53d6c0ef132165a431595c160ac20422567f
jgj1018 <jgj1018@gmail.com>
2018-07-21 22:56:00 +0900

    (1) missing file for the previous commit

--- front/src/vo/Transaction.js
+++ front/src/vo/Transaction.js
@@ -1,9 +1,10 @@
 
 export default class Transaction {
-  constructor (userId, transactionName, costAmount, transactionType) {
+  constructor (userId, transactionName, creditType, debitType, costAmount) {
     this.user_id = userId
     this.transaction_name = transactionName
+    this.credit_type = creditType
+    this.debit_type = debitType
     this.cost_amount = costAmount
-    this.transaction_type = transactionType
   }
 }
