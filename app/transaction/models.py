f43bee2cabccecee983e9db26b7a31d53b9009c4
jgj1018 <jgj1018@gmail.com>
2018-08-11 21:18:25 +0900

    add basic level of validator

--- app/transaction/models.py
+++ app/transaction/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.urls import reverse
-
+from .validator import *
 
 # Whenever any transaction happens, The row is added to this table.
 
 
 class Transaction(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, validators=[positive_int_type])
     # Primary Key necessary, Or auto generate Id column
     # Only Primary Key can be autoField
     transaction_id = models.BigAutoField(primary_key=True)
     transaction_name = models.CharField(max_length=20)
-    cost_amount = models.IntegerField()
-    credit_type = models.SmallIntegerField()
-    debit_type = models.SmallIntegerField()
+    cost_amount = models.IntegerField(validators=[positive_int_type])
+    credit_type = models.SmallIntegerField(validators=[type_in_range])
+    debit_type = models.SmallIntegerField(validators=[type_in_range])
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     # only one auto option can be set
     updated_at = models.DateTimeField(auto_now_add=True)
